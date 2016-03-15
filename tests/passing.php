2dff0d1a272f80e703fe68b8b25dd70c1781ae08
tyson-nw <tvanover@psu.ds.pdx.edu>
2016-03-15 12:50:25 -0700

    Added passing test

--- tests/passing.php
+++ tests/passing.php
@@ -6,19 +6,19 @@ $data = json_decode(file_get_contents("generated.json"), TRUE);
 
 $profiles = new OTag('dl',"id='profiles'");
 foreach($data as $value){
-	$profiles($dt = new OTag('dt',array('id'=>$value['_id'],"class"=>$value['gender'])));
+	$profiles->add($dt = new OTag('dt',array('id'=>$value['_id'],"class"=>$value['gender'])));
 	if(!empty($value['tags'])){
-		$dt->addClass(implode(" ", $value['tags'])); 
+		$dt->__set("class", $dt->__get("class")." ". implode(" ", $value['tags'])); 
 	}
 	if(isset($value['picture'])){
-		$dt($img = new OTag('img',array("src"=>$value['picture'],"class"=>['eyecolor']),OTag::DISPLAY_UNPAIRED));
+		$dt->add($img = new OTag('img',array("src"=>$value['picture'],"class"=>['eyecolor'])));
 	}
 	$dt->add($check = new OTag('input',['type'=>'checkbox','name'=>'check_'.$value['index'],'checked'=>$value['isActive'], 'disabled']));
 	$dt->add("<label for='check_$value[index]'>$value[name]</label>");
-	$profiles($dd = OTag::Craft('dd', $value['greeting'], "class='$value[favoriteFruit]'"));
+	$profiles->add($dd = OTag::Craft('dd', $value['greeting'], "class='$value[favoriteFruit]'"));
 }
 
 echo "<h1>Profile Test</h1>";
 echo "<pre>\n";
-echo htmlentities($profiles->toString());
+echo htmlentities($profiles);
 echo "</pre>\n";
