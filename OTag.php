a8e54680202b9ee41044235ba184a66eef28ecc1
jackelking <djarredon@gmail.com>
2016-04-08 11:43:15 -0700

    Added most required docBlock information

--- OTag.php
+++ OTag.php
@@ -1,34 +1,66 @@
 <?php
 /**
+ *	Quick generic HTML generating object.
  *
+ * @author		Tyson Vanover
+ * @author		Daniel J. Arredondo <djarredon@gmail.com>
+ * @copyright	Copyright (c) 2015 Tyson of the Northwest
+ * @license    https://github.com/tyson-nw/OTag/blob/master/LICENSE MIT License
+ * @link       https://github.com/tyson-nw/OTag
  **/
 class OTag extends OTagObject{
-
-	//$dt->addClass(implode(" ", $value['tags']));
-	//This class appends to the end of the $attribute[class]
+	/**
+	 * Appends to the end of the "class" string in $attribute
+	 * with either a String or an
+	 * @param string $string	The string to append to the list of attributes
+	 * @return void		return value not used
+	 * @access	public
+	 **/
 	public function addClass($string) {
+		/*
+		 * If there is a previously existing "class" attribute then $string is
+		 * appended to that string, otherwise attribute['class'] is set to
+		 * the incoming string
+		 */
 		if (isset($attribute['class']))
 			$attribute['class'] .= " ".$string;
 		else
 			$attribute['class'] = $string;
 	}
-	
-	//returns new instance of OTag given data to copy
+
+	/**
+	 * returns new instance of OTag given data to copy
+	 * @param object $tag	the tag to add
+	 * @param string $content the content to add
+	 * @param array $attributes	the attributes to add
+	 * @param boolean $display=self::DISPLAY_BLOCK  Whether to display the block of text
+	 * @return object	the OTag created with the given data
+	 * @access	public
+	 * @static
+	 **/
 	public static function Craft($tag=NULL, $content="", $attributes=array(), $display=self::DISPLAY_BLOCK){
+		/*
+		 * check if content is an array instead of a string
+		 * iteratively add content to $new
+		 */
 		$new = new OTag($tag, $attributes, $display);
-		//check if content is an array instead of a string
-		//iteratively add content to $new
 		if (is_array($content)) {
 			foreach($content as $var)
 				$new($var);
 		}
-		//else, add string
 		else
 			$new($content);
 		return $new;
 	}
 
-	//default constructor
+	/**
+	 * default constructor
+	 * @param object $tag	the tag to set
+	 * @param mixed $attributes	the attributes to set
+	 * 				This can be either a string or an array
+	 * @param boolean $display=self::DISPLAY_BLOCK  Whether to display the block of text
+	 * @access	public
+	 **/
 	public function __construct($tag=NULL, $attributes=array(), $display=self::DISPLAY_BLOCK){
 		$this->tag = $tag;
 		$this->display = $display;
@@ -42,51 +74,98 @@ class OTag extends OTagObject{
 	}
 }
 
+/**
+ * Class OTagObject
+ */
 abstract class OTagObject{
+	/**
+	 *
+     */
 	const FORMAT = "<%s%s>%s</%s>";
+	/**
+	 *
+     */
 	const PAIRED = "&nbsp";
-	const DISPLAY_UNPAIRED = NULL;	//boolean. True if tag is <br /> or other tag with no paired closing
+	/**
+	 * boolean. True if tag is <br /> or other tag with no paired closing
+     */
+	const DISPLAY_UNPAIRED = NULL;
+	/**
+	 *
+     */
 	const EMPTY_FORMAT = "<%s%s />";
+	/**
+	 *
+     */
 	const DISPLAY_BLOCK = FALSE;
+	/**
+	 *
+     */
 	const DISPLAY_INLINE = TRUE;
-	public static $quote = "'";			//
-	public static $indent = 0;			//spaces to indent
-	public static $indent_char = "\t";	//indent character
-	public static $nl_char = "\n";		//new line character
+	/**
+	 * @var string
+     */
+	public static $quote = "'";
+	/**
+	 * number of spaces to indent
+	 * @var int
+     */
+	public static $indent = 0;
+	/**
+	 * indent character
+	 * @var string
+     */
+	public static $indent_char = "\t";
+	/**
+	 * new line character
+	 * @var string
+     */
+	public static $nl_char = "\n";
+	/**
+	 * @var bool
+     */
 	protected static $outputting = FALSE;
 
-	public $tag;			//<div>/<ul><p>/(HTML tag), ex: "p", "h", "h2"
+	/**
+	 * <div>/<ul><p>/(HTML tag), ex: "p", "h", "h2"
+	 * @var
+     */
+	public $tag;
+	/**
+	 * @var null
+     */
 	public $empty = NULL;
-	protected $attributes;			//HTML attributes, ex: array("style"=>"width: 20em;"),
-	//"class='header'", "style='font-style: italic;'"
-	protected $contents = array();	//Usually a block of text. Ex: a paragraph
-	protected $parents = array();	//
-	protected $display;				//
+	/**
+	 * HTML attributes, ex: array("style"=>"width: 20em;"),"class='header'",
+	 * 								"style='font-style: italic;'"
+	 * @var
+     */
+	protected $attributes;
+	/**
+	 * Usually a block of text. Ex: a paragraph
+	 * @var array
+     */
+	protected $contents = array();
+	/**
+	 * @var array
+     */
+	protected $parents = array();
+	/**
+	 * @var
+     */
+	protected $display;	
 
+	/**
+	 * OTagObject constructor.
+     */
 	abstract public function __construct();
-	//abstract public static function Craft();
 
-	//adds content to the object
-	/* This is not done in __invoke(), This function name
-		may be reused.
-	
-	public function add($content){
-		if($content instanceof OTagObject){
-			$content->_registerParent($this);
-			$this->_checkLoop($content);
-			$this->contents[] = $content;
-		}else{
-			$this->contents[] = $content;
-		}
-		return $content;
-	}
-	*/
-
-	//http://php.net/manual/en/language.oop5.magic.php
-	//This function should replace the add function
-	//should do everything that the add function currently does.
-	//replace the current add function so that we can use "add"
-	//for something else later
+	/**
+	 * @param $content 
+	 * @return mixed
+	 * @throws Exception
+	 * @access public
+     */
 	public function __invoke($content)
 	{
 		if($content instanceof OTagObject){
@@ -98,8 +177,13 @@ abstract class OTagObject{
 		}
 		return $content;
 	}
-
-	//sets $attribute and $content variables
+	
+	/**
+	 * sets $attribute and $content variables
+	 * @param mixed $attribute the attributes to be set
+	 * @param array $content the array of contents to be set
+	 * @access public
+	 */
 	public function __set($attribute, $content){
 		if($content===TRUE){
 			$this->attributes[]=$attribute;
@@ -110,6 +194,11 @@ abstract class OTagObject{
 		}
 	}
 
+	/**
+	 * @param mixed $attribute
+	 * @access public
+	 * @return null
+     */
 	public function __get($attribute){
 		if(isset($this->attributes[$attribute])){
 			return $this->attributes[$attribute];
@@ -117,8 +206,13 @@ abstract class OTagObject{
 		return NULL;
 	}
 
+	/**
+	 * Generates HTML out of the data given
+	 * @access public
+	 * @return string	The data in HTML format
+     */
 	public function __toString(){
-		reset($this->contents);		//reset pointer to beginning of contents
+		reset($this->contents);
 		$first = FALSE;		//
 		if(self::$outputting){
 			self::$indent++;
@@ -129,8 +223,6 @@ abstract class OTagObject{
 		$attribs = "";
 		$attrib_format = " %s=".self::$quote."%s".self::$quote;
 		foreach($this->attributes as $key=>$value){
-			//var_dump($key);
-			//var_dump($value);
 			if($value === true) {
 				$attribs .= " " . $key;
 			}
@@ -208,13 +300,22 @@ abstract class OTagObject{
 	}
 
 	//TODO: anonamyze the function and roll into __tostring.
-	private static function _nl($count,$nl=NULL,$i=NULL){
+	/**
+	 * @param int 		$count	description
+	 * @param string 	$nl		newline character
+	 * @param string 	$i		indent character
+	 * @return string	new line character with $count number of indent characters appended
+     */
+	private static function _nl($count, $nl=NULL, $i=NULL){
 		if($count<0){$count=0;}
 		if(empty($nl)){$nl = self::$nl_char;}
 		if(empty($i)){$i = self::$indent_char;}
 		return $nl.str_repeat($i,$count);
 	}
 
+	/**
+	 * @param object $parent
+     */
 	protected function _registerParent($parent){
 		if(count($this->parents)){
 			trigger_error("Tag '" . $this->tag . "' has been added to second parent '" . $parent->tag ."'");
@@ -222,6 +323,10 @@ abstract class OTagObject{
 		$this->parents[] = $parent;
 	}
 
+	/**
+	 * @param object $child
+	 * @throws Exception
+     */
 	protected function _checkLoop($child){
 		if($child===$this){
 			throw new Exception("A Tag has been made it's own parent");
@@ -233,6 +338,10 @@ abstract class OTagObject{
 	/*
     *http://stackoverflow.com/questions/30199001/how-can-i-parse-an-attribute-string-to-an-array-in-php
     */
+	/**
+	 * @param string $input
+	 * @return array
+     */
 	protected static function _parse_attributes($input){
 		$re = "/(?:\\s*(\\w+)\\s*=\\s*(?:'((?:[^'\\\\]|\\\\')*)'|\"((?:[^\"\\\\]|\\\\\")*)\"?|(\\w+)))/";
 
